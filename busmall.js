828c9beac027a0174b11f1046bcc32e804ed3635
RivaD2 <rivkadavidowski@gmail.com>
2020-07-20 20:37:21 -0700

    added more comments in code

--- busmall.js
+++ busmall.js
@@ -34,26 +34,26 @@ Product.prototype.renderProduct = function() {
   productPText.textContent = this.imgName;
   productPlace.appendChild(productPText);
   target.appendChild(productPlace);
-  this.timesShown++;
+  this.timesShown++; // had to include times shown as a prop on object so that all products timesShown is incremented
 };
 // call render Product
 
 
-// step 3:
+// step 3: //created event handler that wil check to see if the product src matches targeted src
 function handleClickOnProductImg(event) {
   console.log(event.target);
   for(var i =0; i < productArray.length;i++) {
     console.log('checking product', productArray[i]);
     if(productArray[i].src === event.target.getAttribute('src')) {
-      productArray[i].numOfVotes++;
-      totalClicks++;
+      productArray[i].numOfVotes++; //each time the loop iterates through, one will add to num of votes on clicked image
+      totalClicks++; // each time loops iterates thorough, total clicks will also increment
       console.log('it\'s a match', totalClicks);
     }
   }
   displayProducts();
   var totalClicksHtml = document.getElementById('totalClicks');
   totalClicksHtml.innerHtml= totalClicks;
-  //check if rounds meet max amount of clicks of 25
+  // Below check if rounds meet max amount of clicks of 25(diplayRounds var)
   //remove event listener
   //render results
   if(totalClicks === displayRounds) {
@@ -70,9 +70,9 @@ productList.addEventListener('click', handleClickOnProductImg);
 
 function displayProducts() {
   productList = document.getElementById('productList');
-  productList.innerHTML = '';
+  productList.innerHTML = ''; // we set this to an empty string so that old images will be erased 
   var randomizerOne = randomImg(0, productArray.length - 1);
-  var randomizerTwo = randomImg(0, productArray.length - 1);
+  var randomizerTwo = randomImg(0, productArray.length - 1); // index of array starts at 0 and the highest index of array will equal length-1.
   var randomizerThree = randomImg(0, productArray.length - 1);
   var newProductOne = productArray[randomizerOne];
   var newProductTwo = productArray[randomizerTwo];
@@ -81,13 +81,13 @@ function displayProducts() {
   newProductTwo.renderProduct();
   newProductThree.renderProduct();
 }
-function randomImg(min, max) { // Took getRandomNum and we turned it into a method
-  min = Math.ceil(min); // The same code still works in the method
-  max = Math.floor(max);// THe only thing I had to change was the function to the method, and line34 to this.getRandom
+function randomImg(min, max) { // this function will take a max and min indexes and randomize number
+  min = Math.ceil(min);
+  max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-new Product('r2d2', 'busMall-Images/bag.jpg',0,0);
+new Product('r2d2', 'busMall-Images/bag.jpg',0,0); // here I created instances of multiple objects
 new Product('banana', 'busMall-Images/banana.jpg',0,0);
 new Product('tpstand', 'busMall-Images/bathroomtpstand.jpg',0,0);
 new Product('boots', 'busMall-Images/boots.jpg',0,0);
@@ -95,11 +95,11 @@ new Product('breakfast', 'busMall-Images/breakfast.jpg',0,0);
 new Product('bubblegum', 'busMall-Images/bubblegum.jpg',0,0);
 
 function renderTotalVotes() {
-  var target = document.getElementById('productList');
+  var target = document.getElementById('productList'); // this functions loop iterates through product list and renders total votes with image
   for(var i = 0; i < productArray.length; i++) {
     var product = productArray[i];
     var productPlace = document.createElement('li');
     productPlace.textContent = product.imgName + ' had' + product.numOfVotes + ' and was shown' + product.timesShown + ' times.';
-    target.appendChild(productPlace);
+    target.appendChild(productPlace); // had to write in the num of votes, plus times shown plus product name
   }
 }
